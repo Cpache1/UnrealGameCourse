366f2240a8601693f940a18bc671afa43e5a072c
Cpache1 <capacheco111@outlook.com>
2021-11-09 12:53:49 +0000

    Character, Input and Movement
    - Added Character class.
    - Added MoveForward/Turn Input
    - Added Content folders.
    
    (Lecture 2 Complete)

---
+++ Source/UnrealGameCourse/UnrealGameCourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealGameCourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGAMECOURSE_API AUnrealGameCourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
